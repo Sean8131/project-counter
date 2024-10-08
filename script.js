591522039d88210dc50cf2fb623434b923d2bc10
Sean <seanselbyy@gmail.com>
2024-10-08 16:19:17 +1300

    Step 8 - Code refactored and comments added

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-console.log("hey");
-
 let count = 0;
 
-function alertUser() {
-    count++;
-    let label = document.getElementById("display-counter");
-    label.innerText = `Clicked ${count} ${count === 1 ? 'time' : 'times'}`;
-  }
+function incrementCounter() {
+  count++;
+  let label = document.getElementById("display-counter");
+  label.innerText = `Clicked ${count} ${count === 1 ? "time" : "times"}`;
+}
+// The incrementCounter function increments the counter by 1 each time the "Add one more button" is clicked. If the count is 1, then the counter should read "Clicked 1 time", otherwise, the counter should read "Clicked <X> times".
 
-const clicked = document.getElementById("main-button");
+const addOneMore = document.getElementById("counter-button");
+// The addOneMore variable is being assigned the result of document.getElementById("main-button");
 
-clicked.addEventListener("click", alertUser);
+addOneMore.addEventListener("click", incrementCounter);
+// When the user clicks the "counter-button", the incrementCounter function will be called.
