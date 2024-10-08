7b65685d43cf62848d6305b91f0d3889945d2901
Sean <seanselbyy@gmail.com>
2024-10-08 14:31:21 +1300

    added alert to onClick button

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("hey");
+
+function alertUser () {
+    alert("This is an alert");
+}
+
+const clicked = document.getElementById("main-button");
+
+clicked.addEventListener('click', alertUser);
