f1b317aa46c1e86e4c2a61a465ad9779987ce4b5
Sean <seanselbyy@gmail.com>
2024-10-08 16:06:28 +1300

    got 'clicked 1 time' working

--- script.js
+++ script.js
@@ -2,19 +2,12 @@ console.log("hey");
 
 let count = 0;
 
-let count = 0;
-
-function alertUser () {
+function alertUser() {
     count++;
     let label = document.getElementById("display-counter");
-    label.innerText = `Clicked ${count} times`; 
-    count++;
-    let label = document.getElementById("display-counter");
-    label.innerText = `Clicked ${count} times`; 
-}
+    label.innerText = `Clicked ${count} ${count === 1 ? 'time' : 'times'}`;
+  }
 
 const clicked = document.getElementById("main-button");
 
-clicked.addEventListener('click', alertUser);
-
-
+clicked.addEventListener("click", alertUser);
